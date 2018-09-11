8ad008ff0ffd792d07575e1ff279835f367b8d40
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-09-10 23:27:45 -0500

    search bar feature working

--- client/components/home.js
+++ client/components/home.js
@@ -1,27 +1,44 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {getGifsThunk} from '../store'
+import { Form, Input, Button } from 'semantic-ui-react'
 
 class Home extends React.Component {
-  componentDidMount() {
-    this.props.getGifs();
+  constructor(props){
+    super(props);
+    this.state = {
+      inputAnswer: "",
+    }
+  }
+  handleSubmit = () => {
+    this.props.getGifs(this.state.inputAnswer);
+  }
+
+  handleChange = (evt) => {
+    this.setState({inputAnswer: evt.target.value})
   }
 
   render() {
     const gifs = this.props.gifs;
     return (
       <div>
-        {gifs ? gifs.map(gif =>
-          <div key={gif.id} >
-            <img src={gif.images.downsized.url} />
-          </div>
+        <Form onSubmit={this.handleSubmit}>
+          <Input placeholder='Search...' onChange={this.handleChange} />
+          <Button type="submit">Submit</Button>
+        </Form>
+        {
+          gifs ? gifs.map(gif =>
+            <div key={gif.id} >
+              <img src={gif.images.downsized.url} />
+            </div>
           ):
-          <h1>No</h1>
+          null
         }
       </div>
     )
   }
 }
+
 /**
  * CONTAINER
  */
@@ -33,8 +50,8 @@ const mapState = ({gifs}) => {
 
 const mapDispatch = dispatch => {
   return {
-    getGifs() {
-      return dispatch(getGifsThunk())
+    getGifs(query) {
+      return dispatch(getGifsThunk(query))
     }
   }
 }
--- client/store/gif.js
+++ client/store/gif.js
@@ -18,9 +18,9 @@ const getGifs = gifs => ({type: GET_GIFS, gifs})
 /**
  * THUNK CREATORS
  */
-export const getGifsThunk = () => async dispatch => {
+export const getGifsThunk = (query) => async dispatch => {
   try {
-    const res = await Axios.get('http://api.giphy.com/v1/gifs/search?q=kittens&api_key=3Scw7m82zJbfSn8aipUENSrHBRCTd2Ta');
+    const res = await Axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=3Scw7m82zJbfSn8aipUENSrHBRCTd2Ta`);
     const data = await res.data.data;
     return await dispatch(getGifs(data || defaultGifs))
   } catch (err) {
