a44319b957f55b4764e342ce211b0daf186539a4
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-09-12 01:32:52 -0500

    updated the styling for images to be centered and styled up search bar

--- client/components/home.js
+++ client/components/home.js
@@ -21,20 +21,43 @@ class Home extends React.Component {
 
   render() {
     const gifs = this.props.gifs;
+    const bodyStyle = {
+      margin: "0 auto",
+      width: "80%",
+    }
+    const imgPackStyle = {
+      display: "flex",
+      flexWrap: "wrap",
+      margin: "0 auto",
+      justifyContent: "center",
+    }
+    const imgStyle = {
+      margin: "5px",
+      alignItems: "flex-start",
+      display: "flex",
+      flexWrap: "wrap",
+    }
+    const inputStyle = {
+      width: "900px"
+    }
     return (
-      <div>
-        <Form onSubmit={this.handleSubmit}>
-          <Input placeholder='Search...' onChange={this.handleChange} />
-          <Button type="submit">Submit</Button>
-        </Form>
-        {
-          gifs ? gifs.map(gif =>
-            <div key={gif.id} >
-              <img src={gif.images.downsized.url} />
-            </div>
-          ):
-          null
-        }
+      <div style={bodyStyle}>
+        <div>
+          <Form onSubmit={this.handleSubmit}>
+            <Input style={inputStyle} placeholder='Search...' onChange={this.handleChange} />
+            <Button color="green" type="submit">Submit</Button>
+            <br />
+          </Form>
+        </div>
+        <br />
+        <div style={imgPackStyle}>
+          {
+            gifs ?
+            gifs.map(gif =>
+                <img style={imgStyle} key={gif.id} src={gif.images.downsized.url} />
+            ) : null
+          }
+        </div>
       </div>
     )
   }
--- client/store/gif.js
+++ client/store/gif.js
@@ -1,4 +1,4 @@
-import Axios from 'Axios'
+import axios from 'axios'
 
 /**
  * ACTION TYPES
@@ -20,7 +20,7 @@ const getGifs = gifs => ({type: GET_GIFS, gifs})
  */
 export const getGifsThunk = (query) => async dispatch => {
   try {
-    const res = await Axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=3Scw7m82zJbfSn8aipUENSrHBRCTd2Ta`);
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=3Scw7m82zJbfSn8aipUENSrHBRCTd2Ta`);
     const data = await res.data.data;
     return await dispatch(getGifs(data || defaultGifs))
   } catch (err) {
